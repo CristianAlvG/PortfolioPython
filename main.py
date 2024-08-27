c3e7b1fc5ca9fc8f7997005061cd0f185313a539
CristianAlvG <cristian98x@hotmail.com>
2024-08-27 15:47:12 -0600

    App description, title, images and links

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide", page_title="Cristian's Portfolio")
 
 col1, col2 = st.columns(2)
 
@@ -27,14 +27,20 @@ content2 = """Below you can find some of the apps I have built in Python. Feel f
 
 st.write(content2)
 
-col3, col4 = st.columns(2)
-
 df = pandas.read_csv("data.csv", sep=";")
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
